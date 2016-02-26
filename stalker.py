4352d70a5b844026a48c61eea10848a3b758bb7d
Alejandro Fernández Piqué <Alex@MacBook-Pro-de-Alejandro.local>
2016-02-26 14:53:43 +0100

    stalker module adapted for 0 mentions

--- stalker.py
+++ stalker.py
@@ -1,5 +1,7 @@
 def stalker(json):
     counter = json["mentions"]
     num_most_common = counter.most_common(1)[0][1]
+    if json["number_mentions"]==0:
+        return 0
     result = float(num_most_common) / json["number_mentions"] * 100
     return result
