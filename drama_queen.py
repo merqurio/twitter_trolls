f1d5ebef0758b47579d5d34f57f4244e82107c24
alberttrias <albert_4_3@hotmail.com>
2016-02-28 21:26:30 +0100

    Updated drama_queen

--- drama_queen.py
+++ drama_queen.py
@@ -20,7 +20,7 @@ def drama_queen(json):
     if float(json["days_account"]) == 0:
         activity = 0
     else:
-        activity = len(json["tweets"]) / float(json["days_account"])
+        activity = float(json["user_json"]["statuses_count"]) / json["days_account"]
 
     hashtags_per_tweet = float(json["number_hashtags"]) / len(json["tweets"])
     mentions_per_tweet = float(json["number_mentions"]) / len(json["tweets"])
