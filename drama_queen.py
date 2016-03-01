7b66c54748c778c4aca031e93937cbfb3341863c
Gabriel de Maeztu <gabriel.maeztu@gmail.com>
2016-03-01 15:12:56 +0100

    py3 drama queen + documentation

--- drama_queen.py
+++ drama_queen.py
@@ -1,40 +1,43 @@
 import re
 
 
-def drama_queen(json):
+def drama_queen(user):
+    """ Different values to evaluate if it's a dramaqueen
+
+    :param user: the twitter @username data
+    :type user: dict
+
+    :return: Use of signs,
+             Use of capital letter,
+             Activity of user,
+             OMG usage
+    :rtype: float, float, float, float
+    """
+
     num_signs = 0
     num_capitals = 0
     num_char = 0
     num_omg = 0
 
-    for tweet in json["tweets"]:
+    for tweet in user["tweets"]:
         text = tweet["text"]
-        num_signs += len(re.findall(r'\!|\:|\^|\<|\?|\...', text))
-        num_capitals += len(re.findall(r'[A-Z]', text))
-        num_char += len(re.findall(r'\S', text))
-        num_omg += len(re.findall(r' OMG |Oh My God|OH MY GOD|oh my god| omg |o m g|O M G', text))
+        num_signs += len(re.findall(r"\!|\:|\^|\<|\?|\...", text))
+        num_capitals += len(re.findall(r"[A-Z]", text))
+        num_char += len(re.findall(r"\S", text))
+        num_omg += len(re.findall(r"OMG|Oh\sMy\sGod|OH\sMY\sGOD|oh\smy\sgod|omg|o\sm\sg|O\sM\sG", text))
 
     if num_char == 0:
         signs_per_char = 0
         capitals_per_char = 0
     else:
-        signs_per_char = float(num_signs) / float(num_char)*100
-        capitals_per_char = float(num_capitals) / float(num_char)*100
+        signs_per_char = num_signs / num_char * 100.0
+        capitals_per_char = num_capitals / num_char * 100.0
 
-    if float(json["days_account"]) == 0:
+    if float(user["days_account"]) == 0:
         activity = 0
     else:
-        activity = json["user_json"]["statuses_count"] / float(json["days_account"])
+        activity = user["user_json"]["statuses_count"] / float(user["days_account"])
 
-    percentage_tweet_with_omg = float(num_omg)/len(json["tweets"])
+    percentage_tweet_with_omg = float(num_omg)/len(user["tweets"])
 
-    # volem que faci servir moltes majuscules i molts signes de puntuacio
-    # volem que tingui una activitat elevada
-    # volem tambe que faci servir molts hashtags
-    # volem tambe que faci moltes mencions a cada tweet i que el percentatge de tweet amb mencio sigui elevat
-    # quins valors hi donem?
-
-    return signs_per_char, \
-           capitals_per_char, \
-           activity, \
-           percentage_tweet_with_omg
+    return signs_per_char, capitals_per_char, activity, percentage_tweet_with_omg
