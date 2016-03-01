156de2465b1bd798b2777c9ed4e8b966836bc56f
Alejandro Fernández Piqué <alex_bcn.92@hotmail.com>
2016-03-01 20:19:56 +0100

    percentatge_drama_queen function modified

--- manual_model.py
+++ manual_model.py
@@ -13,40 +13,61 @@ api = tweepy.API(auth)
 
 
 def percentage_drama_queen(activity, percentage_tweet_with_omg, capitals_per_char, signs_per_char, percentage_tweet_with_hashtag, percentage_tweet_with_mention, mentions_per_tweet, hashtags_per_tweet):
-    activity_average = 10
-    capitals_per_char_average = 11.13
-    signs_per_char_average = 3.002
-    percentage_tweet_with_omg_average = 0.00259
+    activity_average = 8
+    capitals_per_char_average = 0.1113
+    signs_per_char_average = 0.03002
+    percentage_tweet_with_omg_average = 0.1196
     percentage_tweet_with_mention_average = 0.5950
     percentage_tweet_with_hashtag_average = 0.1758
     mentions_per_tweet_average = 0.7953
     hashtags_per_tweet_average = 0.3431
+    compt1 = 0
+    compt2 = 0
 
     activity -= activity_average
-    if activity > 40:
-        activity = 40
+    if activity > 20:
+        activity = 20
     capitals_per_char -= capitals_per_char_average
     signs_per_char -= signs_per_char_average
     percentage_tweet_with_omg -= percentage_tweet_with_omg_average
     percentage_tweet_with_hashtag -= percentage_tweet_with_hashtag_average
     percentage_tweet_with_mention -= percentage_tweet_with_mention_average
-    mentions_per_tweet -= mentions_per_tweet_average
+    mentions_per_tweet /= mentions_per_tweet_average
+    hashtags_per_tweet /= hashtags_per_tweet_average
+
     if mentions_per_tweet > 5:
         mentions_per_tweet = 5
-    hashtags_per_tweet -= hashtags_per_tweet_average
     if hashtags_per_tweet > 5:
         hashtags_per_tweet = 5
-    result = max(activity, 1)**1.15 + 12 * max(np.exp(percentage_tweet_with_omg)-1, 0) + np.sqrt(max(capitals_per_char,
-            0)) + 2 * np.sqrt(max(signs_per_char, 0))+0.5 * np.sqrt(100 * max(percentage_tweet_with_hashtag, 0))+0.5 * \
-            np.sqrt(100 * max(percentage_tweet_with_mention, 0)) + max(hashtags_per_tweet, 0)**1.5 + \
-             max(mentions_per_tweet, 0)**1.5
+    if activity > 0:
+        compt1 += 1
+    if capitals_per_char > 0:
+        compt1 += 1
+    if signs_per_char > 0:
+        compt1 += 1
+    if percentage_tweet_with_omg > 0:
+        compt1 += 1
+    if percentage_tweet_with_hashtag > 0:
+        compt2 += 1
+    if percentage_tweet_with_mention > 0:
+        compt2 += 1
+    if mentions_per_tweet > 1:
+        compt2 += 1
+    if hashtags_per_tweet > 1:
+        compt2 += 1
+    result = 1.5*max(activity, 0) + 90*max(np.exp(percentage_tweet_with_omg)-1, 0) + 100 * np.sqrt(max(capitals_per_char, 0)) + 100 * np.sqrt(max(signs_per_char, 0))+1.2 * hashtags_per_tweet**1.15+mentions_per_tweet**1.15
+    percentage = float(result)/52*100
 
-    normalizer = 40**1.15 + 12 * np.exp(1-percentage_tweet_with_omg_average)-1 + np.sqrt(100-capitals_per_char_average) \
-                 + 2 * np.sqrt(100-signs_per_char_average)+0.5 * np.sqrt(100 * (1-percentage_tweet_with_hashtag_average)
-                )+0.5 * np.sqrt(100 * (1-percentage_tweet_with_mention_average)) + (5-hashtags_per_tweet_average)**1.5 + \
-                 (5-mentions_per_tweet_average)**1.5
+    if compt1 >= 3 and compt2 >= 2:
+        percentage *= (1+0.20*(compt1+compt2)/8)
 
-    return float(result)/normalizer*100
+    if compt1 <= 1:
+        percentage *= (1-0.35*(8-(compt1+compt2))/8)
+
+    if percentage > 100:
+        percentage = 100
+
+    return percentage
 
 
 def percentage_bot(periodicity, answer, diversity_tweets):
@@ -66,12 +87,14 @@ def percentage_bot(periodicity, answer, diversity_tweets):
 
 def percentage_stalker(num_stalker, who_stalker, mentions_per_tweet, percentage_tweet_with_mention):
     famous = 0
-    if num_stalker > 50:
+    if num_stalker > 45:
         if data_user(str(who_stalker), api)["user_json"]["verified"] == "True":
             famous = 1
+            print "Stop stalking", who_stalker
             return num_stalker, famous
-        if num_stalker > 85:
+        if num_stalker > 75:
             result = num_stalker
+            print "Stop stalking", who_stalker
         else:
             result = (6*num_stalker+3*100*percentage_tweet_with_mention-1*100*mentions_per_tweet)/8
     else:
@@ -80,7 +103,6 @@ def percentage_stalker(num_stalker, who_stalker, mentions_per_tweet, percentage_
 
 
 def percentage_spammer(diversity_tweets, diversity_hashtags, urls_percentage):
-    print diversity_tweets, diversity_hashtags, urls_percentage
     if diversity_tweets == -1:
         return 0
     if diversity_hashtags == -1:
@@ -115,5 +137,7 @@ def run(user):
                                                  mentions_per_tweet, hashtags_per_tweet)
         per_bot = percentage_bot(periodicity, answer, diversity_tweets)
         per_stalker, famous = percentage_stalker(num_stalker, who_stalker, mentions_per_tweet, percentage_tweet_with_mention)
+        if per_stalker == 0:
+            per_stalker = num_stalker
         per_spammer = percentage_spammer(diversity_tweets, diversity_hashtags, urls_percentage)
         print(per_drama_queen, per_bot, per_stalker, per_spammer)
