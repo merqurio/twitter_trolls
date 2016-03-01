65377a54c4bc9d3de6fefda34f633e72918adac9
Alejandro Fernández Piqué <alex_bcn.92@hotmail.com>
2016-03-01 11:50:35 +0100

    solved error in stalker

--- stalker.py
+++ stalker.py
@@ -1,6 +1,6 @@
 def stalker(json):
     counter = json["mentions"]
     if json["number_mentions"] == 0:
-        return 0
+        return 0, "zero"
     result = float(counter.most_common(1)[0][1]) / json["number_mentions"] * 100
     return result, counter.most_common(1)[0][0]
