281ad12d8097532ffa6d9b6098bcd73362146f07
kterf <edu.ribas.92@gmail.com>
2016-03-02 14:47:23 +0100

    debuging

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from manual_model import troll_bot_analyzer
 from connexion import NoContent
 
-
 def get_user(user_id):
+	print("Starting troll_bot_analyzer")
     u = troll_bot_analyzer(user_id)
+	print("troll_bot_analyzer finished")
     if u:
         return u
     else:
