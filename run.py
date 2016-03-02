0c811e5e855a4edee02ef4da42bed2709476e5aa
kterf <edu.ribas.92@gmail.com>
2016-03-02 15:04:58 +0100

    port

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import connexion
 app = connexion.App(__name__, specification_dir='swagger/')
 app.add_api('swagger.yaml')
 #application = app.app
-app.run(port=80, server="tornado")
+app.run(server="tornado", port=8080)
