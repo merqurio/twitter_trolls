9b84fc99ca3a7e022041f296e1bd3391ea3d8dfc
kterf <edu.ribas.92@gmail.com>
2016-03-02 15:11:35 +0100

    app and run

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import connexion
 
 app = connexion.App(__name__, specification_dir='swagger/')
 app.add_api('swagger.yaml')
-#application = app.app
-app.run(port=8080)
+application = app.app
+#app.run(port=8080)
