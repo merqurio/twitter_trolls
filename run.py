98c4b14bc6a7c488d995a16057d6a5cb5d289767
kterf <edu.ribas.92@gmail.com>
2016-03-02 15:48:29 +0100

    port

--- run.py
+++ run.py
@@ -4,6 +4,5 @@ from flask.ext.cors import CORS
 app = connexion.App(__name__, specification_dir='swagger/')
 app.add_api('swagger.yaml')
 #application = app.app
-app.run(port=80)
-
 cors = CORS(app.app, resources={r"/v1/*": {"origins": "*"}})
+app.run(port=80)
